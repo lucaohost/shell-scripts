18b99bcf1ca07c713a7ca584e3ad23ef83afb45d
lucaxrl <lucas.reginatto.de.lima@gmail.com>
2019-05-29 22:22:14 -0300

    Make the code really works
    - In the last time, I was trolled by myself,
    when I insert the SU password for give all privileges to edit the
    file. After this, I runned the code, and thought that worked ...
    - Now, this code works

--- codes/lampp_start.sh
+++ codes/lampp_start.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-sudo /opt/lampp/lampp start
-#!/usr/bin/expect
-set pass "mysecret"
-
-spawn /usr/bin/passwd
-
-expect "password: "
-send "$pass"
+echo "myPassword"|sudo -S /opt/lampp/lampp start
